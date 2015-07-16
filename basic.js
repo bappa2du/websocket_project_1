396f97c8ff5c7285885a6115e3a16b6784c18df4
bappa <iftakharul.alam@live.com>
2015-07-16 18:40:22 +0600

    use a help function

--- basic.js
+++ basic.js
@@ -1,4 +1,19 @@
+function printHelp(){
+    console.log("basic.js (c) Iftakhar");
+    console.log("");
+    console.log("usage:");
+    console.log("--help      print this help");
+    console.log("--name      say hello to {NAME}");
+    console.log("");
+}
+
 var args = require("minimist")(process.argv.slice(2),{string:"name"});
+
+if(args.help || !args.name){
+    printHelp();
+    process.exit();
+}
+
 var name = args.name;
 
 console.log("welcome home "+ name);
